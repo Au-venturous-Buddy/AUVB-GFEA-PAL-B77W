00d94968b43f74e9b0b1ab8fd14f6c88601d23d3
Au-venturous-Buddy <auventurous.buddy@gmail.com>
2026-01-10 18:39:35 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.b77w.pal.gfea.auvb',
+  appName: "Au-venturous Buddy Gotta Fly 'Em All: Philippine Airlines B777-300ER",
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
